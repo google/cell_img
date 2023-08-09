76784d3b57eba51eab3ba6f00481eee4919b156d
Peter Hawkins <phawkins@google.com>
2023-08-09 12:04:58 -0700

    [JAX] Replace uses of `jnp.array` in types with `jnp.ndarray`.
    `jnp.array` is a function, not a type:
    https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.array.html
    so it never makes sense to use `jnp.array` in a type annotation.
    
    Presumably the intent was to write `jnp.ndarray` aka `jax.Array`. Change uses of `jnp.array` to `jnp.ndarray`.
    
    PiperOrigin-RevId: 555234480

--- cell_img/dose_response/constrain.py
+++ cell_img/dose_response/constrain.py
@@ -5,32 +5,32 @@ import jax.numpy as jnp
 
 
 def to_positive(
-    unconstrained: jnp.array,
-) -> jnp.array:
+    unconstrained: jnp.ndarray,
+) -> jnp.ndarray:
   """Constrain an unconstrained array to being positive."""
   return jnp.exp(unconstrained)
 
 
 def to_positive_inverse(
-    constrained: jnp.array,
-) -> jnp.array:
+    constrained: jnp.ndarray,
+) -> jnp.ndarray:
   """Invert to_positive."""
   return jnp.log(constrained)
 
 
 def to_bounded(
-    unconstrained: jnp.array,
+    unconstrained: jnp.ndarray,
     minimum: float,
     maximum: float,
-    ) -> jnp.array:
+) -> jnp.ndarray:
   """Constrain an unconstrained array to lie between minimum and maximum."""
   return (maximum - minimum) * jax.scipy.special.expit(unconstrained) + minimum
 
 
 def to_bounded_inverse(
-    constrained: jnp.array,
+    constrained: jnp.ndarray,
     minimum: float,
     maximum: float,
-    ) -> jnp.array:
+) -> jnp.ndarray:
   """Invert to_bounded."""
   return jax.scipy.special.logit((constrained - minimum) / (maximum - minimum))
